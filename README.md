d850e75ac4662f2f0d5500f6b888c3a5de184b35
bangood <2419281635@qq.com>
2019-03-16 23:02:02 +0800

    搭建开发环境

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# bg-logger" 
+[![travis build](https://img.shields.io/travis/bangood/bg-logger.svg)](https://travis-ci.org/bangood/bg-logger)
---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+let expect = require('chai').expect;
+import a from './index'
+describe('index', function(){
+    it('should work!', function(){
+        expect(a).to.be.equal(2);
+    });
+});
